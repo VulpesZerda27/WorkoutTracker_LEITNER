d6e2b4a062eb9701e77cf66b3f3a32c46e287a20
VulpesZerdes <martin@dankeschatz.com>
2023-03-12 17:26:59 +0100

    wrote main

--- src/FileUtils.java
+++ src/FileUtils.java
@@ -1,14 +1,12 @@
 import java.io.*;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Scanner;
 
 public class FileUtils {
     private Long idCounter = 1L;
     private final String PATH_TO_PERSON_IMPORT = "data/personData.csv";
-    private final String PATH_TO_STATISTICS = "";
+    private final String PATH_TO_STATISTICS = "data/statistics.txt";
 
     public List<Person> readPersonsFromCsv() {
         List<Person> personList = new ArrayList<>();
@@ -27,7 +25,52 @@ public class FileUtils {
         }
 
     public void writeStatisticsToFile(List<Person> persons, List<Workout> workouts){
+        FileWriter fileWriter;
+        try {
+            fileWriter = new FileWriter(new File(PATH_TO_STATISTICS));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        PrintWriter printWriter = new PrintWriter(fileWriter);
+        int personCounter = 1;
 
+        for (Person person:persons) {
+            printWriter.printf("- - - Person %d - - -\n", personCounter);
+            personCounter += 1;
+            printWriter.printf("Name: %s %s\n", person.getFirstName(), person.getLastName());
+            printWriter.printf("Number of biking exercises: %d\n", getNumberOfBikingWorkoutsByPerson(person.getId(), workouts));
+            printWriter.printf("Number of swimming exercises: %d\n", getNumberOfSwimmingWorkoutsByPerson(person.getId(), workouts));
+            printWriter.printf("Average Duration: %.0f minutes\n", getAverageDurationOfWorkoutByPerson(person.getId(), workouts));
+            printWriter.println();
+        }
+
+        List<BikingWorkout> bikingWorkouts = new ArrayList<>();
+        for (Workout workout:workouts) {
+            if(workout instanceof BikingWorkout)
+                bikingWorkouts.add((BikingWorkout) workout);
+        }
+
+        printWriter.printf("- - - Biking - - -\n");
+        printWriter.printf("Average Distance: %.0fm\n", getAverageDistance(bikingWorkouts));
+        printWriter.printf("Average Duration: %.0f minutes\n", getAverageDuration(bikingWorkouts));
+        printWriter.printf("# mountain: %d\n", getNumberOfBikingWorkoutsByType(bikingWorkouts, BikingType.MOUNTAIN));
+        printWriter.printf("# road: %d\n", getNumberOfBikingWorkoutsByType(bikingWorkouts, BikingType.ROAD));
+        printWriter.println();
+
+        List<SwimmingWorkout> swimmingWorkouts = new ArrayList<>();
+        for (Workout workout:workouts) {
+            if(workout instanceof SwimmingWorkout)
+                swimmingWorkouts.add((SwimmingWorkout) workout);
+        }
+
+        printWriter.printf("- - - Swimming - - -\n");
+        printWriter.printf("Average Distance: %.0f\n", getAverageDistance(swimmingWorkouts));
+        printWriter.printf("Average Duration: %.0f\n", getAverageDuration(swimmingWorkouts));
+        printWriter.printf("# backstroke: %d\n", getNumberOfSwimmingWorkoutsByType(swimmingWorkouts, SwimmingType.BACKSTROKE));
+        printWriter.printf("# butterfly: %d\n", getNumberOfSwimmingWorkoutsByType(swimmingWorkouts, SwimmingType.BUTTERFLY));
+        printWriter.println();
+
+        printWriter.close();
     }
     public int getNumberOfSwimmingWorkoutsByType(List<SwimmingWorkout> workouts, SwimmingType type){
         int counter = 0;
@@ -47,7 +90,7 @@ public class FileUtils {
         }
         return counter;
     }
-    public double getAverageDistance (List<Workout> workouts){
+    public double getAverageDistance (List<? extends Workout> workouts){
         double workoutCounter = 0.0;
         double sumOfDistances = 0.0;
 
@@ -59,7 +102,7 @@ public class FileUtils {
         double averageDistance = sumOfDistances/workoutCounter;
         return averageDistance;
     }
-    public double getAverageDuration(List<Workout> workouts) {
+    public double getAverageDuration(List<? extends Workout> workouts) {
         double workoutCounter = 0.0;
         double sumOfDurations = 0.0;
 
@@ -83,7 +126,7 @@ public class FileUtils {
     public int getNumberOfBikingWorkoutsByPerson(Long personId, List<Workout> workoutList) {
         int bikeWorkoutCounter = 0;
         for (Workout workout : workoutList) {
-            if (workout.getPersonId() == personId) {
+            if (workout.getPersonId() == personId && workout instanceof BikingWorkout) {
                 bikeWorkoutCounter += 1;
             }
         }
@@ -92,7 +135,7 @@ public class FileUtils {
     public int getNumberOfSwimmingWorkoutsByPerson(Long personId, List<Workout> workoutList) {
         int swimmingWorkoutCounter = 0;
         for (Workout workout : workoutList) {
-            if (workout.getPersonId() == personId) {
+            if (workout.getPersonId() == personId && workout instanceof  SwimmingWorkout) {
                 swimmingWorkoutCounter += 1;
             }
         }
--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,29 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
         FileUtils fileUtils = new FileUtils();
         List<Person> personList = fileUtils.readPersonsFromCsv();
+        List<Workout> workoutList = new ArrayList<>();
 
-        System.out.println(personList.get(0).age);
+        workoutList.add(new BikingWorkout(1L, 20, 5690, 1L, BikingType.ROAD));
+        workoutList.add(new SwimmingWorkout(2L, 50, 303, 1L, SwimmingType.BACKSTROKE));
+        workoutList.add(new BikingWorkout(3L, 30, 3900, 1L, BikingType.MOUNTAIN));
+        workoutList.add(new SwimmingWorkout(4L, 15, 200, 1L, SwimmingType.BACKSTROKE));
+        workoutList.add(new BikingWorkout(5L, 20, 8290, 1L, BikingType.MOUNTAIN));
+        workoutList.add(new SwimmingWorkout(6L, 45, 150, 2L, SwimmingType.BACKSTROKE));
+        workoutList.add(new BikingWorkout(7L, 72, 3250, 2L, BikingType.MOUNTAIN));
+        workoutList.add(new SwimmingWorkout(8L, 45, 400, 2L, SwimmingType.BUTTERFLY));
+        workoutList.add(new BikingWorkout(9L, 34, 2209, 2L, BikingType.ROAD));
+        workoutList.add(new SwimmingWorkout(10L, 13, 604, 2L, SwimmingType.BACKSTROKE));
+        workoutList.add(new BikingWorkout(11L, 22, 4980, 3L, BikingType.MOUNTAIN));
+        workoutList.add(new SwimmingWorkout(12L, 18, 450, 3L, SwimmingType.BACKSTROKE));
+        workoutList.add(new BikingWorkout(13L, 57, 8906, 3L, BikingType.MOUNTAIN));
+        workoutList.add(new SwimmingWorkout(14L, 48, 680, 3L, SwimmingType.BUTTERFLY));
+        workoutList.add(new BikingWorkout(15L, 9, 2005, 3L, BikingType.ROAD));
 
-       /* Person person1 = new Person(1L, "maria", "maurer", 25);
-        Person person2 = new Person(2L, "markus", "maurer", 32);
-
-        ArrayList<Person> personList = new ArrayList<>();
-        personList.add(person1);
-        personList.add(person2);
-
-        ArrayList<Workout> workoutList = new ArrayList<>();
-        workoutList.add(new BikingWorkout(1L, 20, 50, 1L, BikingType.ROAD));
-        workoutList.add(new BikingWorkout(2L, 40, 20, 1L, BikingType.ROAD));
-
-        FileUtils fileUtils = new FileUtils();
-
-        System.out.println(fileUtils.getAverageDistance(workoutList));
-        System.out.println(fileUtils.getAverageDuration(workoutList));
-        System.out.println(fileUtils.getNumberOfBikingWorkoutsByPerson(1L, workoutList));
-        //fileUtils.getNumberOfBikingWorkoutsByType(workoutList, BikingType.ROAD);*/
+        fileUtils.writeStatisticsToFile(personList, workoutList);
     }
 }
